f59095ba157e81497c773126399030ab192ba71c
Howie tiong <howietiong04@gmail.com>
2026-03-25 02:29:02 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import RoleGuard from '@/components/layout/RoleGuard';
 
 // Pages
 import Home from './pages/Home';
